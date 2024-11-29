fd3f2d2ecba3a1dbee0b09f13a153f884ddf9823
levan1313 <lkhaburzania@onaim.com>
2024-11-29 11:29:39 +0400

    chore: project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
